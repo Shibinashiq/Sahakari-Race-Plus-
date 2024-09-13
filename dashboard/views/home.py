ad76bc623383df30bda5cf25c3439855a0e1ced5
shibinashiq <shibinashiq7224@gmail.com>
2024-09-13 16:59:27 +0530

    comment form  add hyper link

--- dashboard/views/home.py
+++ dashboard/views/home.py
@@ -3,7 +3,8 @@ from dashboard.views.imports import *
 @login_required(login_url='dashboard-login')
 def home(request):
     user=request.user
-    customers = CustomUser.objects.filter(is_deleted=False).exclude(id=user.id).count()
+    customers = CustomUser.objects.filter(is_deleted=False,is_staff=False,is_superuser=False).count()
+    staff = CustomUser.objects.filter(is_deleted=False,is_staff=True,is_superuser=False).count()
     course=Course.objects.filter(is_deleted=False).count()
     subscription = Subscription.objects.filter(is_deleted=False).count()
 
@@ -11,6 +12,7 @@ def home(request):
         "title": "Home | Agua Dashboard",
         "user": user,
         "customers": customers,
+        "staff": staff,
         "course": course,
         "subscription": subscription,
 
